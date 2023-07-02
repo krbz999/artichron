88c2f203fe4931c99141e9564ecc51c941a415b6
Zhell <zhellqol@gmail.com>
2023-07-02 12:59:08 +0200

    add arsenal validation to actor types

--- module/documents/data/actor/actor-merchant.mjs
+++ module/documents/data/actor/actor-merchant.mjs
@@ -4,4 +4,13 @@ export default class MerchantData extends BaseActorModel {
   static defineSchema() {
     return super.defineSchema();
   }
+
+  /* ---------------------------------------- */
+
+  /** Ensure the arsenal worn is valid. */
+  _validateArsenal(update) {
+    const arsenal = foundry.utils.getProperty(update, "system.equipped.arsenal");
+    if (!arsenal) return;
+    if (this.parent.items.get(arsenal.primary)?.isTwoHanded) foundry.utils.setProperty(update, "system.equipped.arsenal.secondary", null);
+  }
 }
--- module/documents/data/actor/actor-monster.mjs
+++ module/documents/data/actor/actor-monster.mjs
@@ -6,4 +6,15 @@ export default class MonsterData extends BaseActorModel {
   static defineSchema() {
     return super.defineSchema();
   }
+
+  /* ---------------------------------------- */
+
+  /** Ensure the arsenal worn is valid. */
+  _validateArsenal(update) {
+    const arsenal = foundry.utils.getProperty(update, "system.equipped.arsenal");
+    if (!arsenal) return;
+    if (this.parent.items.get(arsenal.primary)?.isTwoHanded) foundry.utils.setProperty(update, "system.equipped.arsenal.secondary", null);
+    if (this.parent.items.get(arsenal.secondary)?.isTwoHanded) foundry.utils.setProperty(update, "system.equipped.arsenal.tertiary", null);
+    if (this.parent.items.get(arsenal.tertiary)?.isTwoHanded) foundry.utils.setProperty(update, "system.equipped.arsenal.quaternary", null);
+  }
 }
