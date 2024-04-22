f4e8a28f47f7c005726c34cd62ff39b3042058c3
Zhell <zhellqol@gmail.com>
2024-04-22 20:02:24 +0200

    Add combatant data models

--- module/documents/token/token-document.mjs
+++ module/documents/token/token-document.mjs
@@ -7,6 +7,7 @@ export default class TokenDocumentArtichron extends TokenDocument {
     if (("aura" in flags) || isRedraw) this.object?.drawAura();
   }
 
+  /** @override */
   async _preUpdate(data, options, user) {
     await super._preUpdate(data, options, user);
     if ((options.animate === false)) return;
@@ -16,5 +17,32 @@ export default class TokenDocumentArtichron extends TokenDocument {
     if (!moveX && !moveY) return;
     const ray = new Ray(this, {x: data.x ?? this.x, y: data.y ?? this.y});
     data.rotation = Math.toDegrees(ray.angle) - 90;
+  }
+
+  /** @override */
+  static async createCombatants(tokens, {combat} = {}) {
+    // Identify the target Combat encounter
+    combat ??= game.combats.viewed;
+    if (!combat) {
+      if (game.user.isGM) {
+        const cls = getDocumentClass("Combat");
+        combat = await cls.create({scene: canvas.scene.id, active: true}, {render: false});
+      }
+      else throw new Error(game.i18n.localize("COMBAT.NoneActive"));
+    }
+
+    // Add tokens to the Combat encounter
+    const createData = tokens.reduce((arr, token) => {
+      if (token.inCombat) return arr;
+      arr.push({
+        tokenId: token.id,
+        sceneId: token.parent.id,
+        actorId: token.actorId,
+        hidden: token.hidden,
+        type: (token.actor?.type === "hero") ? "hero" : undefined
+      });
+      return arr;
+    }, []);
+    return combat.createEmbeddedDocuments("Combatant", createData);
   }
 }
