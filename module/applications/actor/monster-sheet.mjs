f3b0581208f497aed4b33831694150da42795678
Zhell <zhellqol@gmail.com>
2024-07-03 22:23:35 +0200

    Allow for dropping items onto the loot tab.

--- module/applications/actor/monster-sheet.mjs
+++ module/applications/actor/monster-sheet.mjs
@@ -148,4 +148,19 @@ export default class MonsterSheet extends ActorSheetArtichron {
     const items = this.document.system.lootDrops;
     return items.sort((a, b) => a.item.name.localeCompare(b.item.name));
   }
+
+  /* -------------------------------------------------- */
+  /*   Event handlers                                   */
+  /* -------------------------------------------------- */
+
+  /** @override */
+  async _onDrop(event) {
+    if (!event.target.closest("[data-application-part=loot]")) return super._onDrop(event);
+
+    const {type, uuid} = TextEditor.getDragEventData(event);
+    if (type !== "Item") return;
+    const item = await fromUuid(uuid);
+    if (item.isEmbedded) return;
+    return this.document.system.addLootDrop(uuid, 1);
+  }
 }
