74fbe47850a0a88341b57ef46a4fd76e161fe1ba
Zhell <kennethzhell@gmail.com>
2024-07-04 11:11:41 +0200

    Allow monsters to use items without equipping them like a PC

--- module/documents/item/item.mjs
+++ module/documents/item/item.mjs
@@ -54,6 +54,7 @@ export default class ItemArtichron extends Item {
    */
   get isEquipped() {
     if (!this.isEmbedded) return false;
+    if (this.actor.type === "monster") return true;
     if (this.isArsenal) return Object.values(this.actor.arsenal).includes(this);
     else if (this.type === "armor") return Object.values(this.actor.armor).includes(this);
     return false;
