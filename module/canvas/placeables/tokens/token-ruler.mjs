dc533ec9722d87dd60db717d58335f0a1744390d
Zhell <kennethzhell@gmail.com>
2025-04-18 19:33:42 +0200

    Remove no-longer needed comment on TokenRuler.

--- module/canvas/placeables/tokens/token-ruler.mjs
+++ module/canvas/placeables/tokens/token-ruler.mjs
@@ -1,10 +1,3 @@
-/**
- * A subclass of the token ruler class that will eventually be responsible for
- * - subtracting AP equal to one fifth of the distance after a token moves.
- * - preventing token movement when it is not the token's combatant's turn or if they cannot afford the movement.
- * - showing the AP cost as a label on the ruler and each waypoint.
- * With v13, these things except the label are found elsewhere.
- */
 export default class TokenRulerArtichron extends foundry.canvas.placeables.tokens.TokenRuler {
   /** @inheritdoc */
   static WAYPOINT_LABEL_TEMPLATE = "systems/artichron/templates/hud/waypoint-label.hbs";
