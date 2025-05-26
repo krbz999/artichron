f5b945996bbb1b89517ed11afb1d67b3a94c6b7e
Zhell <kennethzhell@gmail.com>
2025-05-27 01:48:06 +0200

    Fix not able to toggle effects on documents without system property due to mixin. Make all icons purple.

--- module/documents/base-document-mixin.mjs
+++ module/documents/base-document-mixin.mjs
@@ -17,7 +17,7 @@ export default base => {
 
     /** @inheritdoc */
     getEmbeddedDocument(embeddedName, id, { invalid = false, strict = false } = {}) {
-      const systemEmbeds = this.system.constructor.metadata.embedded ?? {};
+      const systemEmbeds = this.system?.constructor.metadata.embedded ?? {};
       if (embeddedName in systemEmbeds) {
         const path = systemEmbeds[embeddedName];
         return foundry.utils.getProperty(this, path).get(id, { invalid, strict }) ?? null;
