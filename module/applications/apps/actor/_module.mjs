704e735d1a3618f82ea5c173c1b693faacd4190a
Zhell <kennethzhell@gmail.com>
2025-07-15 21:59:03 +0200

    Add recovery phase config.

--- module/applications/apps/actor/_module.mjs
+++ module/applications/apps/actor/_module.mjs
@@ -3,6 +3,7 @@ export { default as MerchantConfigurationDialog } from "./merchant-configuration
 export { default as PartyDistributionDialog } from "./party-distribution-dialog.mjs";
 export { default as PartyFundsDialog } from "./party-funds-dialog.mjs";
 export { default as RecoveryPhase } from "./recovery-phase.mjs";
+export { default as RecoveryPhaseConfig } from "./recovery-phase-config.mjs";
 export { default as SkillRollDialog } from "./skill-roll-dialog.mjs";
 
 export * as configs from "./configs/_module.mjs";
